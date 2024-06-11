e23731423f1c99d8e36143ee2fecd600ccb8a791
Rick Farias <167890284+Rickfarias@users.noreply.github.com>
2024-06-11 16:01:20 -0300

    att

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 81
+console.log(`A velocidade do seu carro é de: ${vel}km/h`)
+if (vel > 80) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre utilizando o cinto de segurança e respeitando as leis de trânsito!`)
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'EUA'
+
+console.log(`Naturalidade: ${pais}.`)
+
+if (pais == 'Brasil') {
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrageiro!')
+}
